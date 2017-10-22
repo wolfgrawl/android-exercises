72e2c14a3fea389178fc1219c099342e541b85dc
Benjamin LACROIX <benjlacroix@gmail.com>
2017-10-22 14:18:28 +0200

    Add Kotlin

---
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -0,0 +1,38 @@
+package fr.android.androidexercises
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+
+        val messageTextView = findViewById<View>(R.id.messageTextView) as TextView
+        // TODO call setText() on messageTextView
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_library, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        return if (id == R.id.action_settings) true else super.onOptionsItemSelected(item)
+    }
+}
