53f70f45c8d54e16a132c19d29d9fae9b858e3dd
Elie Gourdeau <sdsdsd>
2017-11-09 15:21:47 +0100

    OK

--- app/src/main/java/fr/android/androidexercises/Book.java
+++ app/src/main/java/fr/android/androidexercises/Book.java
@@ -2,7 +2,10 @@ package fr.android.androidexercises;
 
 // TODO use parcel
 
-class Book {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+class Book implements Parcelable {
 
     private String name;
     private String author;
@@ -12,11 +15,39 @@ class Book {
         this.author = author;
     }
 
+    protected Book(Parcel in) {
+        name = in.readString();
+        author = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
     public String getName() {
         return name;
     }
 
     String getAuthor() {
         return author;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(author);
     }
 }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -19,16 +22,29 @@ public class LibraryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button openButton = (Button) findViewById(R.id.openButton);
+        Button dateButton = (Button) findViewById(R.id.dateButton);
 
-        Book book = new Book("Garry Whopper", "CK Rowling");
+        final Book book = new Book("Garry Whopper", "CK Rowling");
 
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
-                // TODO Add book to intent
+                intent.putExtra("BOOK", book);
                 startActivity(intent);
             }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                Toast.makeText(LibraryActivity.this, dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_SHORT).show();
+                            }
+                        }, 2015, 9 ,1).show();
+                    }
         });
     }
 
