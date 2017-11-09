be1e7a62c2951d966558761bfb7ba4b74f5817b1
Elie Gourdeau <sdsdsd>
2017-11-09 11:40:43 +0100

    button efficient

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -25,6 +26,8 @@ public class LibraryActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Replace with startActivity() to start BookActivity
                 Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
+                startActivity(intent);
             }
         });
     }
